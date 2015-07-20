ea86378db0fba9ca544c691546cedc994d0728c6
Meredith Pretorius and Sam Coward <pair+mpretorius+scoward@pivotallabs.com>
2015-07-20 13:04:50 -0400

    Initial Commit

---
+++ Catty/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Catty
+//
+//  Created by pivotal on 7/20/15.
+//  Copyright © 2015 kingcoward. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Catty/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Catty
+//
+//  Created by pivotal on 7/20/15.
+//  Copyright © 2015 kingcoward. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
