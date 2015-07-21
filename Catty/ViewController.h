3fa4d147cd58fd967e1b8e65bed5a704db89214f
Meredith Pretorius and Sam Coward <pair+mpretorius+scoward@pivotallabs.com>
2015-07-21 11:17:41 -0400

    WIP

--- Catty/ViewController.h
+++ Catty/ViewController.h
@@ -1,11 +1,3 @@
-//
-//  ViewController.h
-//  Catty
-//
-//  Created by pivotal on 7/20/15.
-//  Copyright © 2015 kingcoward. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
--- Catty/ViewController.m
+++ Catty/ViewController.m
@@ -1,27 +1,47 @@
-//
-//  ViewController.m
-//  Catty
-//
-//  Created by pivotal on 7/20/15.
-//  Copyright © 2015 kingcoward. All rights reserved.
-//
-
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIView *blueBar;
+@property (weak, nonatomic) IBOutlet UIView *purpleBar;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    [self scheduleBlue];
+    [self schedulePurple];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - private
+
+- (void)scheduleBlue {
+    [self performSelector:@selector(executeBlue) withObject:nil afterDelay:5.0];
+}
+
+- (void)schedulePurple {
+    [self performSelector:@selector(executePurple) withObject:nil afterDelay:1.0];
+}
+
+- (void)executeBlue {
+    [UIView animateWithDuration:0.25
+                     animations:^{
+                         self.blueBar.hidden = !self.blueBar.hidden;
+                     }
+                     completion:^(BOOL finished) {
+                         [self scheduleBlue];
+                     }];
+}
+
+- (void)executePurple {
+    [UIView animateWithDuration:0.25
+                     animations:^{
+                         self.purpleBar.hidden = !self.purpleBar.hidden;
+                     }
+                     completion:^(BOOL finished) {
+                         [self schedulePurple];
+                     }];
 }
 
 @end
